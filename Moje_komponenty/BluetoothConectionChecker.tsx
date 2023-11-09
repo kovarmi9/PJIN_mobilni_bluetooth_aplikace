3b960c504d1366179a7f24b03d77362e4d118b8b
Michal Kovář <kovarmi9@seznam.cz>
2023-11-09 21:38:31 +0100

    přidána kontrola připojení bluetooth zařízení kter á se zatím nikde neaplikuje

---
+++ Moje_komponenty/BluetoothConectionChecker.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react';
+import { BleManager } from 'react-native-ble-plx';
+
+const useBluetoothConnectionChecker = () => {
+  const [isDeviceConnected, setIsDeviceConnected] = useState(false);
+
+  useEffect(() => {
+    const manager = new BleManager();
+
+    const checkDeviceConnection = async () => {
+      const devices = await manager.connectedDevices([]);
+      setIsDeviceConnected(devices.length > 0);
+    };
+
+    checkDeviceConnection();
+  }, []);
+
+  return isDeviceConnected;
+};
+
+export default useBluetoothConnectionChecker;

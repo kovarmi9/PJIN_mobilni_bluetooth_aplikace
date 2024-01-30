d0a16f13eab7d8d4c5e0c0cb983bf0e2073cf2ed
Michal Kovář <kovarmi9@seznam.cz>
2024-01-31 00:24:14 +0100

    odstraněny přebytečné části kodu

--- App.tsx
+++ App.tsx
@@ -1,38 +1,17 @@
 // import knihoven
 import React from 'react';
-import type {PropsWithChildren} from 'react';// když je takhle zešedlá tak se momentálně nepoužívá ale zatím jsem je tu nechal kdyby se ještě někdy hodila
 import  {useState,useEffect} from 'react';
 import {
   SafeAreaView,
   ScrollView,
   StatusBar,
-  StyleSheet,
   Text,
   useColorScheme,
   View, 
-  Linking, 
-  Alert, 
   Dimensions,
 } from 'react-native';
 
-import {
-  Colors,
-} from 'react-native/Libraries/NewAppScreen';
-
-// chtěl jsem přidat nějaké hezké ikony ale nefungovalo mi to zajsem zatím použi emoji
-// import ikon z: npm install react-native-vector-icons
-// dále je třeba dát do terminálu: npm install --save-dev @types/react-native-vector-icons
-//import Icon from 'react-native-vector-icons/FontAwesome';
-
-
-// import knihovny pro práci s bluetooth npm install --save react-native-ble-plx
-// ANDROID_APP_projekt\android\app\src\main\AndroidManifest.xml přidáno
-//<uses-permission android:name="android.permission.BLUETOOTH"/>
-//<uses-permission android:name="android.permission.BLUETOOTH_ADMIN"/>
-//<uses-permission android:name="android.permission.ACCESS_COARSE_LOCATION"/>
-//<uses-permission android:name="android.permission.BLUETOOTH_SCAN"/>
-//<uses-permission android:name="android.permission.BLUETOOTH_CONNECT"/>
-// importuju pro čtení a celkovou komunikaci přes bluetooth
+import { Colors,} from 'react-native/Libraries/NewAppScreen';
 import { BleManager } from 'react-native-ble-plx';
 
 // import mých vlastních komponent
@@ -57,9 +36,6 @@ function App(): JSX.Element {
 
   const screenHeight = Dimensions.get('window').height;
 
-  //For BLE Permissions
-  //const {requestPermissions, scanForDevices, allDevices} = useBLE();
-
   // Přidána stavová proměnná pro sledování stavu připojení
   const [connected, setConnected] = useState(false);
 
@@ -76,58 +52,32 @@ function App(): JSX.Element {
     })
     setIsModalVisible(true)
   };
-  
-  const manager = new BleManager();
 
-  manager.startDeviceScan(null, null, (error, device) => {
-    if (error) {
-      console.log(error);
-      return;
-    }
-  
-    // Přidána kontrola null
-    if (device && device.name === 'Niceboy HIVE pods 2') {
-      manager.stopDeviceScan();
-  
-      device.connect()
-        .then((device) => {
-          console.log('Připojeno k zařízení', device.name);
-        })
-        .catch((error) => {
-          console.log('Chyba při připojování k zařízení', error);
-        });
-    }
-  });
+  // Požadování oprávnění
+  useEffect(() => {
+    requestPermissions((isGaranted:boolean) => {
+      //alert('The Android Permission is Garanted? ', isGaranted);
+    })
+  }, []);
+
+  const [deviceName, setDeviceName] = useState("");
 
   useEffect(() => {
     const manager = new BleManager();
-
+  
     manager.startDeviceScan(null, null, (error, device) => {
       if (error) {
         console.log(error);
         return;
       }
     
-      // Přidána kontrola null
-      if (device && device.name === 'Niceboy HIVE pods 2') {
-        manager.stopDeviceScan();
-    
-        device.connect()
-          .then((device) => {
-            console.log('Připojeno k zařízení', device.name);
-            setConnected(true);  // Aktualizace stavu připojení
-          })
-          .catch((error) => {
-            console.log('Chyba při připojování k zařízení', error);
-            setConnected(false);  // Aktualizace stavu připojení
-          });
+      // Kontrola, zda bylo detekováno zařízení
+      if (device) {
+        console.log('Detekováno zařízení', device.name);
       }
     });
   }, []);
   
-  
-
-
   // věci co vraci hlavní komponenta 
   return (
     // obsah uvnitř SafeAreaView bere ohledy na notch
@@ -159,14 +109,12 @@ function App(): JSX.Element {
 			  onPress={handleSouboryPress}
 			/>
 
-      {/*<Text style={{textAlign: 'center'}}>{isDeviceConnected ? "Zařízení je připojeno" : "Zařízení není připojeno"}</Text>*/}
-
       <Text style={{textAlign: 'center'}}>{connected ? 'Zařízení je připojeno' : 'Zařízení není připojeno'}</Text>
 
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={{...backgroundStyle, height: screenHeight-217}}>
-        <Radek nazev="ZAZNAM_.TXT" datum="31.10.2023 14:15"/>
+        <Radek nazev="ZAZNAM_1.TXT" datum="31.10.2023 14:15"/>
         <Radek nazev="ZAZNAM_2.TXT" datum="31.10.2023 14:13"/>
         <Radek nazev="ZAZNAM_3.TXT" datum="31.10.2023 14:10"/>
         <Radek nazev="ZAZNAM_4.TXT" datum="31.10.2023 12:27"/>
